9e6ed94889eeccfed03e5f90b3ae34cc564549ac
MINWOO KIM <sunnybikers@naver.com>
2025-10-17 17:46:42 +0900

    refactor: long 타입 접미사 추가
    "0"만 쓰면 int로 인식하기에 "0L"로 접미사 L 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,27 +1,27 @@
 package calculator;
 
 public class Calculator {
-    private long result;
-    private PreProcessor preProcessor;
+    private static long result;
+    private static PreProcessor preProcessor;
 
     public Calculator() {
-        this.result = 0;
-        this.preProcessor = new PreProcessor();
+        result = 0L;
+        preProcessor = new PreProcessor();
     }
 
     public long work(String input) {
-        if (input.isEmpty()) return 0;
+        if (input.isEmpty()) return 0L;
 
         String[] refinedInput = preProcessor.work(input);
         for (String each : refinedInput) {
             try {
                 if (!each.isBlank()) {
-                    this.result += Long.parseLong(each);
+                    result += Long.parseLong(each);
                 }
             } catch (Exception e) {
                 throw new IllegalArgumentException("올바르지 않은 입력 형식입니다.");
             }
         }
-        return this.result;
+        return result;
     }
 }
