602e4ab97fd046b78c6c64380223a6b62843a3d0
MINWOO KIM <rlaalsdn110@gmail.com>
2025-11-03 22:42:18 +0900

    feat: 입력값 파싱 구현
    - 특이케이스에 대한 예외 처리와 일반적인 경우 파싱 기능 구현 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,8 +6,6 @@ import calculator.controller.CalculatorControllerFactory;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("//\\n12+1 = " + "//\n12+1");
-
         CalculatorControllerFactory factory = new CalculatorControllerFactory();
         CalculatorController calculatorController = factory.calculatorController();
         calculatorController.run();
--- src/main/java/calculator/domain/InputString.java
+++ src/main/java/calculator/domain/InputString.java
@@ -2,28 +2,65 @@ package calculator.domain;
 
 import calculator.ErrorMessage;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class InputString {
     private static final int ALL = -1;
-    private List<String> slicedInput;
+    private static final String DELIMITER_FORMAT = "^//(.)\\\\n.*$";
+    private static final String DELIMITER = ":,";
+    private static final String POSSIBLE_FORMAT_PREFIX = "^(//(.)\\\\n)*([";
+    private static final String POSSIBLE_FORMAT_SUFFIX = "]*\\d*)+$";
+
+    private final List<String> slicedInput;
 
     private InputString(List<String> slicedInput) {
         this.slicedInput = slicedInput;
     }
 
     public static InputString from(String input) throws IllegalArgumentException {
-        validate(input);
-//        String
-        List<String> strings = List.of(input.split("", ALL));
+        validateFirst(input);
+        String delimiter = makeDelimiter(input);
+        validateSecond(input, delimiter);
+        input = sliceDelimiters(input);
+
+        List<String> strings = Stream
+                .of(input.split("[" + delimiter + "]", ALL))
+                .filter(s -> !s.isBlank())
+                .toList();
+
         return new InputString(strings);
     }
 
-    private static void validate(String input) throws IllegalArgumentException {
+    private static String makeDelimiter(String input) {
+        String delimiter = DELIMITER;
+        while (input.matches(DELIMITER_FORMAT)) {
+            delimiter += String.valueOf(input.charAt(2));
+            input = input.substring(5);
+        }
+        return delimiter;
+    }
+
+    private static void validateFirst(String input) throws IllegalArgumentException {
         if (input.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.EMPTY_STRING.getMessage());
         }
-        if (!input.matches("^(//\\.\\n)*([:,]*\\d*)+$")) {
+    }
+
+    private static void validateSecond(String input, String delimiter) throws IllegalArgumentException {
+        String possibleFormat = POSSIBLE_FORMAT_PREFIX + delimiter + POSSIBLE_FORMAT_SUFFIX;
+        if (!input.matches(possibleFormat)) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_STRING.getMessage());
         }
+    }
+
+    private static String sliceDelimiters(String input) {
+        while (input.matches(DELIMITER_FORMAT)) {
+            input = input.substring(5);
+        }
+        return input;
+    }
+
+    public List<String> getSlicedInput() {
+        return slicedInput;
     }
 }
