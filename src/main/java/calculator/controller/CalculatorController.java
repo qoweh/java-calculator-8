e6ed72fd0084235161a88c00507fae92ff5b16f5
MINWOO KIM <rlaalsdn110@gmail.com>
2025-11-03 22:50:37 +0900

    refactor: 메서드 분리
    - 한 메서드에서 하나의 책임만 가지도록 메서드를 최대한 분리

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -8,12 +8,19 @@ import camp.nextstep.edu.missionutils.Console;
 public class CalculatorController {
     public void run() {
         InputView.notifyStart();
+
         String input = Console.readLine();
+        InputString inputString = makeValidInputString(input);
+
+    }
+
+    private InputString makeValidInputString(String input) {
+        InputString inputString = null;
         try {
-            InputString inputString = InputString.from(input);
+            inputString = InputString.from(input);
         } catch (IllegalArgumentException e) {
             OutputView.error(e);
         }
-
+        return inputString;
     }
 }
--- src/main/java/calculator/domain/InputString.java
+++ src/main/java/calculator/domain/InputString.java
@@ -6,6 +6,8 @@ import java.util.stream.Stream;
 
 public class InputString {
     private static final int ALL = -1;
+    private static final int TARGET = 2;
+    private static final int BEGIN_INDEX = 5;
     private static final String DELIMITER_FORMAT = "^//(.)\\\\n.*$";
     private static final String DELIMITER = ":,";
     private static final String POSSIBLE_FORMAT_PREFIX = "^(//(.)\\\\n)*([";
@@ -18,15 +20,9 @@ public class InputString {
     }
 
     public static InputString from(String input) throws IllegalArgumentException {
-        validateFirst(input);
         String delimiter = makeDelimiter(input);
-        validateSecond(input, delimiter);
-        input = sliceDelimiters(input);
-
-        List<String> strings = Stream
-                .of(input.split("[" + delimiter + "]", ALL))
-                .filter(s -> !s.isBlank())
-                .toList();
+        validate(input, delimiter);
+        List<String> strings = makeSlicedInput(input, delimiter);
 
         return new InputString(strings);
     }
@@ -34,23 +30,30 @@ public class InputString {
     private static String makeDelimiter(String input) {
         String delimiter = DELIMITER;
         while (input.matches(DELIMITER_FORMAT)) {
-            delimiter += String.valueOf(input.charAt(2));
-            input = input.substring(5);
+            delimiter += String.valueOf(input.charAt(TARGET));
+            input = input.substring(BEGIN_INDEX);
         }
         return delimiter;
     }
 
-    private static void validateFirst(String input) throws IllegalArgumentException {
+    private static void validate(String input, String delimiter) throws IllegalArgumentException {
+        String possibleFormat = POSSIBLE_FORMAT_PREFIX + delimiter + POSSIBLE_FORMAT_SUFFIX;
         if (input.isBlank()) {
             throw new IllegalArgumentException(ErrorMessage.EMPTY_STRING.getMessage());
         }
-    }
-
-    private static void validateSecond(String input, String delimiter) throws IllegalArgumentException {
-        String possibleFormat = POSSIBLE_FORMAT_PREFIX + delimiter + POSSIBLE_FORMAT_SUFFIX;
         if (!input.matches(possibleFormat)) {
             throw new IllegalArgumentException(ErrorMessage.INVALID_STRING.getMessage());
         }
+    }
+
+    private static List<String> makeSlicedInput(String input, String delimiter) {
+        input = sliceDelimiters(input);
+
+        List<String> strings = Stream
+                .of(input.split("[" + delimiter + "]", ALL))
+                .filter(s -> !s.isBlank())
+                .toList();
+        return strings;
     }
 
     private static String sliceDelimiters(String input) {
